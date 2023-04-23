372d8e9b41efb4329d924e1afcab6b998688e712
Sanil Pannachan <spannachan@gmail.com>
2023-04-23 11:32:01 -0500

    i don't know what changes I have made?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+      id: 1,
+    },
+    {
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+      id: 2,
+    },
+    {
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+      id: 3,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
 
 export default App;
+
+// // Using Class Based Component
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello From a Class</h1>
+//   }
+// }
+
+// export default App;
